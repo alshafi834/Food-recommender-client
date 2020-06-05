3d1a54e43c1f98bd2b97330dad7cf4b4a1723436
alshafi834 <alshafi834@gmail.com>
2020-06-05 18:08:00 +0200

    changes

--- src/users/pages/Home.js
+++ src/users/pages/Home.js
@@ -11,7 +11,10 @@ const Home = () => {
         <img src={banner2} className="banner-img" alt="" />
       </div>
       <div className="hero-right">
-        <p>Find medically accurate food according to disease</p>
+        <p>
+          Get the medically accurate food<br></br> suggestion according to your
+          disease
+        </p>
         <button>
           <NavLink to="/auth">Get started</NavLink>
         </button>
