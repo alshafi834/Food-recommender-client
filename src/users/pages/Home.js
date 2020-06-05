81fddec41157b22e706bd3acb162bf1cfa2edc28
alshafi834 <alshafi834@gmail.com>
2020-06-05 21:45:39 +0200

    responsive

--- src/users/pages/Home.js
+++ src/users/pages/Home.js
@@ -7,17 +7,19 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="hero-left">
-        <img src={banner2} className="banner-img" alt="" />
-      </div>
-      <div className="hero-right">
-        <p>
-          Get the medically accurate food<br></br> suggestion according to your
-          disease
-        </p>
-        <button>
-          <NavLink to="/auth">Get started</NavLink>
-        </button>
+      <div className="banner">
+        <div className="hero-left">
+          <img src={banner2} className="banner-img" alt="" />
+        </div>
+        <div className="hero-right">
+          <p>
+            Get the medically accurate food<br></br> suggestion according to
+            your disease
+          </p>
+          <button>
+            <NavLink to="/auth">Get started</NavLink>
+          </button>
+        </div>
       </div>
     </div>
   );
